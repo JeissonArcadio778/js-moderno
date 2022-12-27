7e19316e351877bcbee3483b74b6858b05c8868f
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2022-12-26 19:40:14 -0500

    loops ok

--- 00-ejercicios/03-bucles.js
+++ 00-ejercicios/03-bucles.js
@@ -290,7 +290,185 @@
 
 
 //TODO: Do while loop:
+// Realiza un script que le pida al usuario que telee la letra A (en mayúscula) y mientras no lo haga se lo vuelva a pedir. Usa un bulce do..while y la ventana alert para los mensajes
+// do {
+//     let letter = prompt(); 
+//     if (letter === 'A') {
+//         alert('Good!'); 
+//         break; 
+//     }
+// } while (true);
 
-do {
-  
-} while (letter);
+
+// //Or
+// let letter; 
+// do {
+//     letter = prompt(); 
+// } while (letter != 'A');
+
+// let count = 2, num = parseInt(prompt()), isPrimo = 'yes primo'
+// do {
+//     if (num%count == 0) {
+//         isPrimo = 'No primo'
+//         break; 
+//     }
+//     console.log(count);
+//     count++
+// } while (count <= num );
+
+// alert(isPrimo); 
+
+// En este script vas a crear un menu con tres opciones: escribir, leer, ssalir. El usuario elegirá una opción y en un alert se le indicará la opción elegida. El programa vuelve a mostrar el menu y se repite el proceso. El script acabará cuando se pulse la opción de salir.
+
+// let option; 
+// do {
+//    option = parseInt(prompt(`
+//         1. Comprar una SIM
+//         2. Pasarte a Virgin 
+//         3. salir
+//     `)); 
+
+//     switch (option) {
+//         case 1:
+//             console.log('Comprar SIM');
+//             break;
+//         case 2:
+//             console.log('Pasarte a Virgin');
+//             break;
+//         case 3:
+//             console.log('Vuelve pronto');
+//             break;
+    
+//         default:
+//             alert('Choose a correct answer')
+//             break;
+//     }
+    
+// } while (option != 3);
+
+// En este script se le pide al usuario que teclee dos números, el script mostrará la suma y pedirá al usuario si queire repetir. Si teclea S repite y si teclea N el programa sale. No acepta otras teclas
+// let  i, number, sum, times; 
+// do {
+//     number = 0, sum = 0, times = 0, i = 0; 
+//     console.log({number,sum, times, i});
+//     //cuantas números quiere sumar 
+//     times =  parseInt(prompt('how much numbers do u want to sum?'));
+//     //sumo 
+
+//     while (i < times) {
+//         number = parseInt(prompt('Give me the numbers, please'));
+//         sum += number
+//         i++
+//     }
+//     //muestro
+//     console.log({sum});
+
+//     //desea continuar?
+//     option = prompt('Do u want to try again? Yes/Y - No/N');
+
+// } while (option != 'N');
+
+// Usar un bucle do..while para crear una cadena que contenga letras repetidas. Las letras se entran por teclado y solo podrán ser la X o la Z despues de elegir las letras se le ingresa el número de repeticiones que deberá estar entre 1 y 15.
+// let letter, times = 1000, res;
+
+// do {
+//     do {
+//         letter = prompt('Get a Letter: ');
+//         console.log({letter});
+//     } while (letter != 'X' && letter != 'Z');
+    
+//     console.log({letter});
+
+//     do {
+//         times = parseInt(prompt('Get times: '));
+//         console.log({times}); 
+//     } while (1 > times || times > 15);  
+
+//     res = letter.repeat(times); 
+//     console.log(res); 
+
+// } while (!res);
+
+// En este sript se trata de calcular el máximo común divisor de dos números. El algorimto es simple: dividimos el primoer por el segundo. si el resto es cero este segundo es el mcm. Si no se desecha el primero y se repite el proceso con el segundo y el resto. Así mientras el resto no sea 0
+
+
+// ENtra un número, lo analiza y muestra, sino, sigue con el otro
+// let num, deno, res; 
+// num = parseInt(prompt()); 
+// deno = parseInt(prompt());
+// do {
+//     if (num%deno == 0) {
+//         console.log(`El mcd de ${num} es ${deno}`);
+//         break; 
+//     }
+//     deno--
+// } while (deno != 0);
+
+
+// Este ejemplo debe pedir al usuario un  número entero y, mediante sucesivas divisiones por 10 debe escribir los digitos que lo forman de menor a mayor orden (de unidades hacia arriba) en una cadena separados por un espacio
+
+// let num, dig, arr = [];
+// num = parseInt(prompt()); 
+// do {
+//     console.log({num});
+//     dig = num%10; 
+//     console.log({dig});
+//     num = parseInt(num/10);
+//     console.log(num);
+//     arr.push(dig);
+// } while (num > 0 );
+
+// console.log(`Las unidades: ${arr[0]}, decenas: ${arr[1]}, centenas ${arr[2]}`);
+
+// Diseñar un script que pida un número mayor que cero y devuelva la suma de todos los números pares desde cero al número introducido. Mostrará la suma y volverá a pedir otro núero hasta que el usuario teclee un 0.
+
+// let num, sum, letter; 
+// do {
+//     sum = 0, letter = ''; 
+//     num = parseInt(prompt('get a number. 0 for exit')); 
+//     for (let i = 2; i < num; i +=2) {
+//         letter += i + ' '; 
+//         sum += i; 
+//     }
+//     if (sum === 0) {
+//         break; 
+//     }
+//     console.log({sum});
+//     console.log(letter+=num);
+
+// } while (num != 0);
+
+// Escribir un script usando el bucle do while para pedir que el usuario escriba dos números. Si el primero es mayor que el segundo el programa volverá a pedir que se escriban los números. Si el primero es menor o igual que el segundo el programa acaba y dice "Numeros en orden creciente"
+
+// let numOne, numTwo;
+// do {
+//     numOne = parseInt(prompt())
+//     numTwo = parseInt(prompt())
+
+//     if (numOne <= numTwo) {
+//         console.log("Numeros en orden creciente");
+//         break;
+//     }
+    
+// } while (true);
+
+// Escribir un script para determinar si el numero introducido por el usuario es un palíndromo, es decir, un número que se lee igual del derecho que del revés.
+
+// Para invertir el número podéis hacer sucesivas divisiones entre 10: el resto lo añadis al inveso multiplicado por 10. Y repetis con el valor entero del numero dividido por 10.
+
+// let isPalindromo = false;
+
+// do {
+//     let word = prompt('Give a word: '); 
+//     let arrLetters = word.split(""); //Separo cada letra en "" y convierto en un array
+//     arrLetters.reverse();  
+//     newWord = arrLetters.join(""); //Unir letras por ""
+
+//     if (newWord === word) {
+//         isPalindromo = true
+//         console.log({isPalindromo});
+//         break; 
+//     }
+//     console.log({isPalindromo});
+
+// } while (!isPalindromo);
